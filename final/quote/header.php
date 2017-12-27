e274bb81d5a106db921cd70d75e75a64c8597aee
AllenWu <allen.wu@spaceadvisor.com>
2017-12-27 18:52:10 +0800

    更新 order_cart.html & space.html

--- final/quote/header.php
+++ final/quote/header.php
@@ -5,7 +5,7 @@
 		</a>
 		<a href="" class="activity">找活動</a>
 		<a href="" class="member" style="background-image:url(dist/images/avator.jpg)"></a>
-		<a href="" class="mail"><i class="fa fa-envelope-o" aria-hidden="true"></i><b>1</b></a>
+		<a href="" class="mail"><i class="fa fa-envelope-o" aria-hidden="true"></i>收件夾<b>1</b></a>
 		<select name="select" id="select">
 			<option>中文</option>
 			<option>English</option>
