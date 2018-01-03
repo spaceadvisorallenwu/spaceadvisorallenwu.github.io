66c1222f8e54c2bb4c9b6abf6a144460106d2665
AllenWu <allen.wu@spaceadvisor.com>
2018-01-03 10:18:20 +0800

    調整css和新檔案

---
+++ final/dist/js/main_bear.js
@@ -0,0 +1,28 @@
+//index 類型收合用
+$(function(){
+	$('#style-open').click(function() {
+		$('.style-other').slideToggle(900);
+	});
+	$('.bookList .buttons .payment .btn').click(function() {
+		$(this).parents('.bookList').find('.pay-information').toggle();
+	});
+	$('#style-open2').click(function() {
+		$('.style-other2').slideToggle(0);
+	});
+	$('.bookList .buttons .comment .btn').click(function(){
+		$('.bookList .buttons .comment .comment_list').toggle();
+	});
+
+	$('[name="payselect"]').each(function(idx,obj){
+		$(obj).on('click',function(event){
+			$('.payment .pay-select:visible').not($(event.target).parentsUntil('li').parent().find('.pay-select')).slideToggle(0);
+			$(event.target).parentsUntil('li').parent().find('.pay-select:not(:visible)').slideToggle(0);
+		});
+	});
+	$('[name="invoiceselect"]').each(function(idx,obj){
+		$(obj).on('click',function(event){
+			$('.invoice .invoice-sub:visible').not($(event.target).parentsUntil('li').parent().find('.invoice-sub')).slideToggle(0);
+			$(event.target).parentsUntil('li').parent().find('.invoice-sub:not(:visible)').slideToggle(0);
+		});
+	});
+});
